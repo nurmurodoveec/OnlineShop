5b2b9ec362afc106f4a52ebcfb6b3d164c09d155
nurmurodoveec <137428953+nurmurodoveec@users.noreply.github.com>
2024-03-30 15:09:17 +0500

    start

---
+++ OnlineShop/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineShop.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string shortDescription { get; set; }
+        public string longDescription { get; set; }
+        public ushort price { get; set; }
+        public bool isFavourite { get; set; }
+        public int available { get; set; }
+        public int categoryId { get; set; }
+        public virtual Category Category { get; set; }  
+    }
+}
---
+++ OnlineShop/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string categoryName { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
